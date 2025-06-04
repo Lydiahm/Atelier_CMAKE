115158c9f18fd8fa175a4ed67885f4104198c6ab
Lydiahm <109420511+Lydiahm@users.noreply.github.com>
2025-06-04 10:16:33 +0100

    Update calculator.h

--- include/calculator.h
+++ include/calculator.h
@@ -5,6 +5,6 @@ double _add(double a, double b);
 double _sub(double a, double b);
 double _mul(double a, double b);
 double _div(double a, double b);
-double _car(double x);
+double car(double x);
 
 #endif
