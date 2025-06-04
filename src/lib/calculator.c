a2bbfc2c112d1e7ae001fb3113e4ef37105ad4f7
RABAH MOKRANE <97698768+MassiMokrane@users.noreply.github.com>
2025-06-04 10:27:29 +0200

    Update calculator.c

--- src/lib/calculator.c
+++ src/lib/calculator.c
@@ -1,21 +1,21 @@
 #include "./../../include/calculator.h"
 
 double _add(double a, double b) {
-    return a+b;
+	return a + b;
 }
 
 double _sub(double a, double b) {
-    return a-b;
+	return a - b;
 }
 
 double _mul(double a, double b) {
-    return ab;
+	return a * b;
 }
 
 double _div(double a, double b) {
-    return a/b;
+	return a / b;
 }
 
-int _car(int a) {
+double _car(double a) {
 	return a * a;
 }
