51a9210121004b6525cbe9f3492c174b11ee1397
Lydiahm <109420511+Lydiahm@users.noreply.github.com>
2025-06-04 10:26:52 +0100

    Update calculator.h

--- include/calculator.h
+++ include/calculator.h
@@ -1,15 +1,5 @@
-#ifndef CALCULATOR_H
-#define CALCULATOR_H
-
 double _add(double a, double b);
 double _sub(double a, double b);
 double _mul(double a, double b);
 double _div(double a, double b);
-double car(double x);
-
-#endif
-
-
-
-
-
+double _car(double a); // fonction pour élever au carré
